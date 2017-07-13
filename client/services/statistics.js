09f91b25cf312e9f77300454c3f31d91a5d74911
Federico Bertolini <fb@hellofresh.com>
2017-07-13 23:11:18 +0200

    calculate time to merge

--- client/services/statistics.js
+++ client/services/statistics.js
@@ -1,3 +1,7 @@
+import mean from 'lodash/mean';
+import sum from 'lodash/sum';
+import round from 'lodash/round';
+
 export const getPullRequestsStatistics = (pullRequests, users) => {
     const result = {
         users: {},
@@ -9,22 +13,27 @@ export const getPullRequestsStatistics = (pullRequests, users) => {
         },
     };
 
-    users.forEach(user => {
-        result.users[user] = {
-            pullRequestsCreated: 0,
-        };
-    });
+    const timesToMerge = [];
 
     pullRequests.forEach(pr => {
         incrementUserCreatedPR(result.users, pr);
         incrementState(result.state, pr);
+        addTimeToMerge(timesToMerge, pr);
     });
+
+    result.timeToMerge = calculateTimeToMerge(timesToMerge);
 
     return result;
 };
 
 const incrementUserCreatedPR = (userStates, pr) => {
-    userStates[pr.user.login].pullRequestsCreated++;
+    if (userStates[pr.user.login]) {
+        userStates[pr.user.login].pullRequestsCreated++;
+    } else {
+        userStates[pr.user.login] = {
+            pullRequestsCreated: 1,
+        };
+    }
 };
 
 const incrementState = (state, pr) => {
@@ -38,4 +47,29 @@ const incrementState = (state, pr) => {
     } else {
         console.log(`unknown status ${pr.state}`);
     }
+};
+
+const addTimeToMerge = (times, pr) => {
+    if (pr.merged) {
+        const createdDate = new Date(pr.created_at);
+        const mergedDate = new Date(pr.merged_at);
+        const milliseconds = mergedDate.getTime() - createdDate.getTime();
+        times.push(Math.floor(milliseconds / 1000));
+    }
+};
+
+const calculateTimeToMerge = (secondsToMerge) => {
+    const meanTime = mean(secondsToMerge);
+    const meanDeviation = deviation(secondsToMerge, meanTime);
+
+    return {
+        timesHours: secondsToMerge.map(v => round(v / 3600, 2)),
+        meanHours: round(meanTime / 3600, 2),
+        meanDeviationHours: round(meanDeviation / 3600, 2)
+    };
+};
+
+const deviation = (values, meanValue) => {
+    const diff = values.map(v => Math.abs(v - meanValue));
+    return sum(diff) / diff.length;
 };

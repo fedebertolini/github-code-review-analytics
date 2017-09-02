ce8a21d1bbd651035d9d688ff052e6d20a4cf173
Federico Bertolini <fb@hellofresh.com>
2017-09-02 20:05:35 +0200

    User table: select all checkbox

--- client/components/Wizard/Users/UserTable.js
+++ client/components/Wizard/Users/UserTable.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { is } from 'immutable';
-import { Table } from 'semantic-ui-react';
+import { Table, Checkbox } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import includes from 'lodash/includes';
 import { getUsers, getSelectedUsers } from '../../../store/selectors/user';
+import { selectUser, unselectUser } from '../../../store/actions/user';
 import UserRow from './UserRow';
 import TablePagination from './TablePagination';
 
@@ -14,6 +15,8 @@ class UserTable extends Component {
     componentWillMount() {
         this.isUserSelected = this.isUserSelected.bind(this);
         this.selectPage = this.selectPage.bind(this);
+        this.unselectAll = this.unselectAll.bind(this);
+        this.selectAll = this.selectAll.bind(this);
 
         this.state = {
             filteredUsers: this.props.users,
@@ -43,14 +46,33 @@ class UserTable extends Component {
         this.setState({ currentPage: page });
     }
 
+    unselectAll(users) {
+        users.forEach(user => this.props.unselectUser(user.get('login')));
+    }
+
+    selectAll(users) {
+        users.forEach(user => this.props.selectUser(user.get('login')));
+    }
+
     render() {
         const page = this.state.currentPage;
         const visibleUsers = this.state.filteredUsers.slice(PAGE_SIZE * (page - 1), PAGE_SIZE * page);
+        const areAllChecked = visibleUsers.size > 0 && visibleUsers.every(this.isUserSelected);
+        const areSomeChecked = !areAllChecked && visibleUsers.some(this.isUserSelected);
         return (
             <Table celled compact definition>
                 <Table.Header fullWidth>
                     <Table.Row>
-                        <Table.HeaderCell />
+                        <Table.HeaderCell>
+                            <Checkbox
+                                checked={areAllChecked}
+                                indeterminate={areSomeChecked}
+                                onChange={() => areSomeChecked || areAllChecked ?
+                                    this.unselectAll(visibleUsers) :
+                                    this.selectAll(visibleUsers)
+                                }
+                            />
+                        </Table.HeaderCell>
                         <Table.HeaderCell>User</Table.HeaderCell>
                         <Table.HeaderCell>Contributions</Table.HeaderCell>
                     </Table.Row>
@@ -86,4 +108,9 @@ const mapStateToProps = state => ({
     selectedUsers: getSelectedUsers(state),
 });
 
-export default connect(mapStateToProps)(UserTable);
+const mapDispatchToProps = {
+    unselectUser,
+    selectUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserTable);

7831af18688c72764fbca6fa3948be56e994a2b6
Federico Bertolini <fb@hellofresh.com>
2017-09-03 16:42:09 +0200

    Dashboard refactoring

--- client/components/Dashboard/index.js
+++ client/components/Dashboard/index.js
@@ -2,35 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import subMonths from 'date-fns/sub_months';
 import formatDate from 'date-fns/format';
-import { getRepositoriesPullRequests } from '../../services/pullRequests';
-import { getPullRequestsStatistics } from '../../services/statistics';
 import UserStatistics from './UserStatistics';
 import TotalStatistics from './TotalStatistics';
 import DayStatistics from './DayStatistics';
 import RepositoryStatistics from './RepositoryStatistics';
-import { getSelectedOrganization } from '../../store/selectors/organization';
-import { getSelectedRepositories } from '../../store/selectors/repository';
-import { getSelectedUsers } from '../../store/selectors/user';
-import { loadStatistics } from '../../store/actions/statistics';
+import { fetchStatistics } from '../../store/actions/statistics';
 import './styles.css';
 
 class Dashboard extends Component {
 
-    async componentWillMount() {
-        const { organization, repositories, users } = this.props;
-        const filter = {
-            authors: users.toArray(),
+    componentWillMount() {
+        this.props.fetchStatistics({
             createdFrom: formatDate(subMonths(new Date(), 1), 'YYYY-MM-DD'),
-        };
-        const pullRequests = await getRepositoriesPullRequests(organization, repositories, filter);
-        this.props.loadStatistics(getPullRequestsStatistics(pullRequests));
+        });
     }
 
     render() {
         return (
             <div className="dashboard_page-container">
                 <TotalStatistics />
-                {this.props.repositories.size > 1 && <RepositoryStatistics />}
+                <RepositoryStatistics />
                 <UserStatistics />
                 <DayStatistics />
             </div>
@@ -38,14 +29,8 @@ class Dashboard extends Component {
     };
 }
 
-const mapStateToProps = state => ({
-    organization: getSelectedOrganization(state),
-    repositories: getSelectedRepositories(state),
-    users: getSelectedUsers(state),
-});
-
 const mapDispatchToProps = {
-    loadStatistics,
+    fetchStatistics,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(null, mapDispatchToProps)(Dashboard);
--- client/store/actions/statistics.js
+++ client/store/actions/statistics.js
@@ -1,6 +1,28 @@
 import { STATISTICS_LOAD } from '../constants';
+import { getSelectedOrganization } from './../selectors/organization';
+import { getSelectedRepositories } from './../selectors/repository';
+import { getSelectedUsers } from './../selectors/user';
+import { getRepositoriesPullRequests } from '../../services/pullRequests';
+import { getPullRequestsStatistics } from '../../services/statistics';
 
 export const loadStatistics = statistics => ({
     type: STATISTICS_LOAD,
     payload: statistics,
 });
+
+export const fetchStatistics = ({ createdFrom, createdTo }) => async (dispatch, getState) => {
+    const state = getState();
+    const organization = getSelectedOrganization(state);
+    const repositories = getSelectedRepositories(state);
+    const users = getSelectedUsers(state);
+
+    const filter = {
+        authors: users.toArray(),
+        createdFrom,
+        createdTo
+    };
+
+    const pullRequests = await getRepositoriesPullRequests(organization, repositories, filter);
+    const statistics = getPullRequestsStatistics(pullRequests);
+    dispatch(loadStatistics(statistics));
+};
